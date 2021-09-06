7f5e2d0c2857a12b23d5999052b2cb760ec435ee
NaRae Lee <narae.dev@gmail.com>
2021-09-06 19:43:35 +0900

    탐욕법>큰수만들기(level2)

--- P_code/P_level2_make-biggest-number.py
+++ P_code/P_level2_make-biggest-number.py
@@ -1,4 +1,16 @@
-def solution(number, k):
+def solution(number,k):
+    answer = []
+    for i in range(len(number)):
+        answer.append(number[i])
+        while k>0 and len(answer)>1:
+            if answer[-1]>answer[-2]:
+                answer.pop(-2)
+                k-=1
+            else:
+                break
+    return "".join(answer[:len(answer)-k])
+
+def solution2(number, k):
     answer = []
     for i in range(len(number)):
         if k<0:
@@ -11,9 +23,13 @@ def solution(number, k):
                     if answer[-1]<number[i] and k>0:
                         answer.pop()
                         k-=1
+                    else:
+                        break
                 answer.append(number[i])
             print(number[i], answer)
     answer = list(map(str,answer))
+    if k>0:
+        answer = answer[:-k]
     return "".join(answer)
 
 number = "4177252841"
