2cb979e3385144d067755f74198da85e8deef079
NaRae Lee <narae.dev@gmail.com>
2021-09-08 21:20:30 +0900

    비교 숫자의 길이, 2자리 수의 자리 유의 => 실패

--- not yet/P_level2_most-biggest-number.py
+++ not yet/P_level2_most-biggest-number.py
@@ -1,16 +1,25 @@
 def change(n):
     sn = str(n)
-    if len(sn) == 3:
+    if len(sn) == 4:
+        return sn[:-1]
+    elif len(sn) == 3:
         return sn
-    elif len(sn) == 2:
-        return sn+sn[0]
+    elif len(sn)==2:
+        if sn[0]<sn[1]:
+            return sn+str(int(sn[0])+1)
+        else:
+            return sn+sn[0]
     else:
         return sn*3
+
 def solution(numbers):
-    numbers.sort(key=lambda x:change(x),reverse=True)
+    numbers.sort(key=lambda x:(change(x),len(str(x))),reverse=True)
     numbers = list(map(str, numbers))
-    print(numbers)
+    for i in range(100):
+        for j in range(11):
+            print(numbers[i*10+j],end = ' ')
+        print('')
     return ''.join(numbers)
 
-numbers = [99,999,9,988,889,8,34]
+numbers = [i for i in range(0,1001)]
 print(solution(numbers))
