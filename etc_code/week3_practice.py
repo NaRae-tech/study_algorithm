a3c7121eb23947690df249a7bbc7d3cfd4338542
NaRae Lee <narae.dev@gmail.com>
2021-09-16 22:26:31 +0900

    오픈소스SW개론 3주차 8강실습코드  추가

--- etc_code/week3_practice.py
+++ etc_code/week3_practice.py
@@ -133,7 +133,119 @@ def search(name):
             break
     if not flag:
         print("없음")
-fEx2()
-search('Sun')
-search('Jacob')
-search('Yuna')
+
+#class exerciese1
+class Queue:
+    queue = []
+    def isEmpty(self):
+        return (len(self.queue) == 0)
+    def enqueue(self, num):
+        self.queue.append(num)
+    def dequeue(self):
+        if len(self.queue) != 0:
+            self.queue.pop()
+    def peek(self):
+        if len(self.queue) != 0:
+            return self.queue[0]
+        else:
+            return "None"
+    def queueSize(self):
+        return len(self.queue)
+
+from random import *
+def clEx3():
+    i = 0
+    Q = Queue()
+    while i<10:
+        num = randint(1,2)
+        print("---%d step---" %(i+1))
+        if num == 1:
+            Q.enqueue(i)
+        else:
+            num = Q.dequeue()
+        print("Current queue: ",Q.queue)
+        print("Peek data:", Q.peek())
+        print("Queue size:", Q.queueSize())
+        i+=1
+
+#class exercise2
+class Shelf:
+    box = [[],[],[],[]]
+    def levelSize(self, num):
+        return len(self.box[num])
+    def isFull(self, num):
+        return (True if self.levelSize(num) == 4 else False)
+    def isEmpty(self, num):
+        return (True if self.levelSize(num)== 0 else False)
+    def addNum(self,num):
+        floor = num%4
+        if self.isFull(floor):
+            print("%d층은 꽉차서 더이상 공간이 없습니다."%(floor++1))
+        else:
+            self.box[floor].append(num)
+    def delNum(self,num):
+        floor = -num-1
+        if self.isEmpty(floor):
+            print("%d층은 비어서 제거할 숫자가 없습니다." %(floor+1))
+        else:
+            self.box[floor].pop()
+
+def clEx2():
+    s = Shelf()
+    num =0
+    print("-4 <= N <= -1: 각 층의 최근 입력 숫자 제거, 나머지 음수:종료")
+    while num>-5:
+        num = int(input("0 혹은 양수 입력 :"))
+        if num>=-4 and num<=-1:
+            s.delNum(num)
+        elif num<-4:
+            print("종료합니다.")
+            break
+        else:
+            s.addNum(num)
+        print("--> 현재 box 상태 : ",s.box)
+
+#class exercise3
+class Square:
+    def __init__(self, x, y, length):
+        self.X = x
+        self.Y = y
+        self.L = length
+        self.point1 = [self.X,self.Y]
+        self.point2 = [self.X+self.L,self.Y]
+        self.point3 = [self.X,self.Y+self.L]
+        self.point4 = [self.X+self.L,self.Y+self.L]
+class CommonArea:
+    def __init__(self,s1,s2):
+        if ((s1.point2[0]<=s2.point1[0] or s2.point2[0]<=s1.point1[0])
+        or s1.point3[1]<=s2.point1[1] or s2.point3[1]<=s1.point1[1]):
+            self.area = 0.00
+        elif s1.X<=s2.X and s1.Y<=s2.Y:
+            self.area = abs(s1.point2[0]-s2.point1[0])*abs(s1.point4[1]-s2.point1[1])
+        elif s2.X<=s1.X and x2.Y<=s1.Y:
+            self.area = abs(s2.point2[0]-s1.point1[0])*abs(s2.point4[1]-s1.point1[1])
+        elif s1.X<=s2.X and s1.Y>=s2.Y:
+            self.area = abs(s1.point2[0]-s2.point1[0])*abs(s2.point3[1]-s1.point2[1])
+        elif s2.X <= s1.X and s2.Y >= s1.Y:
+            self.area = abs(s2.point2[0] - s1.point1[0]) * abs(s1.point3[1] - s2.point2[1])
+
+def clEx3():
+    for i in range(2):
+        print("첫 번째 정사각형의 중심좌표와 한 변의 길이를 입력하세요.")
+        x1 = int(input("X좌표 : "))
+        y1 = int(input("Y좌표 : "))
+        l1 = int(input("한 변의 길이 : "))
+        s1 = Square(x1,y1,l1)
+
+        print("\n두 번째 정사각형의 중심좌표와 한 변의 길이를 입력하세요.")
+        x2 = int(input("X좌표 : "))
+        y2 = int(input("Y좌표 : "))
+        l2 = int(input("한 변의 길이 : "))
+        s2 = Square(x2,y2,l2)
+
+        print("첫 번째 정사각형의 좌표, 넓이 : ", [x1,y1],l1**2)
+        print("두 번째 정사각형의 좌표, 넓이 : ", [x2,y2],l2**2)
+        ca = CommonArea(s1,s2)
+        print("두 정사각형의 공통 넓이 : ",ca.area)
+
+clEx3()
